4248577126e788ab735e595679adad9bdb5ad214
Carl <cjcefren00243@usep.edu.ph>
2025-10-06 10:14:03 +0800

    feat: add routes for player selection and player creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import Welcome from './pages/welcome'
 import Play from './pages/Play'
 import Leaderboards from './pages/Leaderboards'
+import PlayerSelection from './pages/PlayerSelection'
+import CreatePlayer from './pages/CreatePlayer'
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Welcome />} />
+          <Route path="/select-player" element={<PlayerSelection />} />
+          <Route path="/create-player" element={<CreatePlayer />} />
           <Route path="/play" element={<Play />} />
           <Route path="/leaderboards" element={<Leaderboards />} />
         </Routes>
